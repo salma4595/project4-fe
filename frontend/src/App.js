41485960420370b08e2048468b6edb3e612b3dae
noor <noormahdi205@gmail.com>
2024-01-18 15:43:18 +0300

    add company folder in the components with the files of it (form of company, companies, join to request and map)and user folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link} from 'react-router-dom'
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import AddCompanyForm from './components/company/AddCompanyForm';
+import JoinRequestForm from './components/company/JoinRequestForm';
+import Companies from './components/company/Companies';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Link className="nav-link text-white d-inline" style={{padding:10}} to="/company/AddCompanyForm">Add Company</Link> &nbsp;
+    <Link className="nav-link text-white d-inline" style={{padding:10}} to="/company/JoinRequestForm">Join As a Company</Link> &nbsp;
+    <Link className="nav-link text-white d-inline" style={{padding:10}} to="/company/Companies">Show Companies</Link> &nbsp;
+ 
+
+    <Routes>
+      <Route path="/company/AddCompanyForm" element={<AddCompanyForm/>} />
+      <Route path="/company/JoinRequestForm" element={<JoinRequestForm/>} />
+      <Route path='/company/Companies' element={<Companies/>} />
+    </Routes>
+    </>
+    
   );
 }
 
