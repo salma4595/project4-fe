acee562c76ab40d6fa310da1b3b52e0ea5ddce0d
lulwa <lulwafelaifel9@gmail.com>
2024-01-21 10:08:23 +0300

    categories

--- frontend/src/components/Category/CategoryEditForm.jsx
+++ frontend/src/components/Category/CategoryEditForm.jsx
@@ -1,76 +1,40 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router';
+
 export default function CategoryEditForm(props) {
-  const [category, setCategory] = useState(props.category);
-  const [file, setFile] = useState();
+const [editCategory, setEditCategory] = useState(props.category);
 
-// console.log("the file image" , file);
-
-  const handleChange = (event) =>{
+const handleChange = (event) => {
     const attributeToChange = event.target.name;
     const newValue = event.target.value;
-    const updatedCategory = {...category}
-    updatedCategory[attributeToChange] = newValue;
-    console.log(updatedCategory);
-    setCategory(updatedCategory);
+    const UpdateCategory = {...editCategory}
+    UpdateCategory[attributeToChange] = newValue;
+    console.log(UpdateCategory);
+    setEditCategory(UpdateCategory);
 }
-
-const handleImage = (event) => {
-    console.log("the updaeted image: ",event.target.files[0]);
-    setFile(event.target.files[0]);
-      // let pic= setFile(event.target.files[0]);
-    // setCategory(category.image = pic);
-    // console.log("set category: in edit , ",setCategory);
-    // console.log("set file " ,     props.setFile(event.target.files[0])    );
-    // setCategory(category.image = updatedImage);
-    console.log("the updated image saved:" ,event.target.files[0]);
-    setCategory((category) => ({
-        ...category,
-        // image:event.target.files[0]
-      }));
-
-}
-
-// console.log("set category in edit Category form",setCategory);
-console.log("logged user" , props.user);
-// console.log("the category" , category);
-// setCategory(category.image = handleImage());
-// console.log("the handle image: , " , setCategory(category.image = handleImage()));
-const handleSubmit = (event) =>{ 
+ const handleSubmit = (event) => {
     event.preventDefault();
-    const formData = new FormData()
-    formData.append("category" , JSON.stringify(category))
-    console.log("category image:" , file);
-    formData.append("image" , file);
-    // formData.append("userId" , props.user.id);
-    //updtaeAuthor is the post 
-    props.updatedCategory(formData)
-    // navigate("/category/index");
-    
-}
-
+    props.UpdateCategory(editCategory);
+    event.target.reset();
+};
 
 return (
-<>
-<h1 className="mt-5">Edit Category:</h1>
-<img src={`/uploads/${category.image}`} alt={category.name}/>
-<form onSubmit={handleSubmit} className="w-75 m-auto mx-auto my-5" encType="multipart/form-data">
-    <div class="mb-3">
-        <label className="mt-3">Category Name:</label>
-        <input type='text' name='name' onChange={handleChange} className="form-control" value={category.name} ></input>
-    </div>
+    <>
+     <h1 className='mt-5'>Edit Category</h1>
+     <form onSubmit={handleSubmit} className="w-75 m-auto my-5 ">
+        <div className='mb-3'>
+            <label className="mt-3">Category Name</label>
+            <input type="text" name='name' value={editCategory.name} onChange={handleChange} className='form-control'></input>
+        </div>
+        <div className="mb-3">
+            <label htmlFor="formFile" className="form-label mt-3">Upload Category Image</label>
+            <input className="form-control" type="file" id="formFile" name="categories_image"></input>
+        </div>
+        <div>
+            <input type="submit" value="Update Category" className="btn btn-sm btn-dark"></input>
+        </div>
 
-    <div class="mb-3">
-        <label for="formFile" class="form-label mt-3">Upload Category Image:</label>
-        <input class="form-control" type="file" id="formFile" name="image"  onChange={handleImage}
-/>
-    </div>
+     </form>
+    </>
 
-    <div>
-        <input type='submit' value="Add Edit" class="btn btn-sm btn-dark"></input>
-    </div>
-
-
-  </form></>
 )
 }
--- frontend/src/components/Category/CategoryList.jsx
+++ frontend/src/components/Category/CategoryList.jsx
@@ -1,145 +1,105 @@
-import React, { useEffect, useState } from 'react'
-import Category from "./Category"
+import React, { useEffect, useState } from 'react';
+import Category from "./Category";
 import  Axios  from 'axios';
-import "bootstrap/dist/css/bootstrap.min.css";
-import CategoryCreateForm from './CategoryCreateForm'
-import CategoryEditForm from './CategoryEditForm'
+import CategoryCreateForm from './CategoryCreateForm';
+import CategoryEditForm from './CategoryEditForm';
 
 
-export default function CategoryList(props) {
 
-    const[categories,setCategories]= useState([]);
+
+export default function CategoryList() {
+    const [categories, setCategories] = useState([]);
     const [isEdit, setIsEdit] = useState(false);
-    const [currentCategory , setCurrentCategory] = useState({});
-    const [file, setFile] = useState();
-    //const [showCreateForm , setShowCreateForm] = useState(false);
+    const [currentCategory, setCurrentCategory] = useState({});
+   
+    useEffect(() => {
+        loadCategoriesList();
+    },[]);
 
-    useEffect(()=>{
-        loadCategoryList();
-    },[])
-
-    const setHeader = () => {
-        const authHeader ={
-        headers:{
-          "Authorization" : "Bearer " + localStorage.getItem("token")
-        }
-      }
-      return authHeader;
-      }
-
-    const loadCategoryList =()=>{
-        Axios.get("/categories/index" , setHeader())
-        .then((response)=>{
-            console.log(response);
+    const loadCategoriesList = () =>{
+       
+         Axios.get("categories/index")
+          .then((response)=>{
             setCategories(response.data.categories);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+            
+    };
+    const addCategory = (Category) => {
+        Axios.post("categories/add",Category)
+        .then(() => {
+            loadCategoriesList();
         })
-        .catch((err)=>{
-            console.log(err)
-        })
-    }
-    
-    const handleCategoryClick = (category) => {
-        console.log("Category clicked:",category);
-    }
- 
-
-    const addCategory = (category) =>{
-        
-        Axios.post("/categories/add",category , setHeader() , { headers: {'Content-Type': 'multipart/form-data'}})
-           .then(res =>{
-            console.log("Category Added successfuly !!");
-            console.log(res);
-            loadCategoryList();
-           })
-           .catch(err =>{
-            console.log("Error adding Category :" , err);
-           })
-        } 
-
-
-
-    const editCategory = (id) => {
-        Axios.get(`/categories/edit?id=${id}` , setHeader())
-        .then((res)=> {
-            console.log("category:" , res.data.category);
-            console.log("loading category successfully");
-            let category = res.data.category;
-               //is edit used to display form
+        .catch((err) => {
+            console.log(err);
+        });
+    };
+    const editView = (id) => {
+        Axios.get(`categories/edit?id=${id}`)
+        .then((res) =>{
+            let category = res.data.categories;
             setIsEdit(true);
-            //current author info
             setCurrentCategory(category);
         })
-        .catch(error=>{
-            console.log("error on editing category in createForm frontend , " , error);
+        .catch((err) => {
+            console.log(err);
+        });
+    };
+    const UpdateCategory = (category) => {
+        Axios.put("categories/update",category)
+        .then(()=>{
+            loadCategoriesList();
         })
-    }
+        .catch((err) => {
+            console.log(err);
+        });
+    };
+    const deleteCategory = (id) =>{
+        Axios.delete(`categories/delete?id=${id}`)
+        .then(()=>{
+            loadCategoriesList();
+        })
+        .catch((err) =>{
+            console.log(err);
+        });
+    };
 
+    const allCategories = categories.map((category,index) =>(
+        <tr key={index}>
+            <Category>
+                name={category.name}
+                image={category.categories_image}
+                editView={() => editView(category._id)}
+                deleteCategory={() => deleteCategory(category._id)}
+            </Category>
 
-   
-        const updatedCategory = (category) => {
-           
-            Axios.put('/categories/update' ,category , { headers: {'Content-Type': 'multipart/form-data'}}, setHeader())
-            .then(res=> {
-                console.log("Category updated successfully frontend");
-                console.log(res);
-                loadCategoryList();
-                setIsEdit(false);
-                loadCategoryList();
-            })
-            .catch(error=>{
-                console.log("error in updating category");
-                console.log(error);
-                console.log("category in category list" , category);
-
-            })
-        }
-
-        const deleteCategory = (id)=> {
-            Axios.delete(`/categories/delete?id=${id}` ,  setHeader())
-            .then(res=>{
-                console.log("category record deleted successfully from frontend");
-                console.log(res);
-                loadCategoryList();
-            })
-            .catch(error=>{
-                console.log("error on deleting category frontend");
-                console.log(error);
-            })
-        }
-
-      const categoryList = categories.map((category)=>(
-     
-        
-                <Category key={category._id} category={category} onClick={handleCategoryClick} editCategory={editCategory} deleteCategory={deleteCategory} ></Category>
-              
-
-    )) 
-    
-
-
+        </tr>
+    ));
 
   return (
-    <>
-     <div>
+    <div>
+    <h1>Categories List</h1> 
 
-        <h3>choose a category</h3>
+       
       
-    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+    <div className="d-grid gap-2 d-md-flex justify-content-md-end">
         {/* <a href="/category/add" class="btn btn-dark me-md-2" type="button">Add Category</a> */}
     </div>       
    <br/>
  <div className="row">
-    {categoryList}
+    {allCategories}
     </div>
+     
+  {(!isEdit)?
+  <CategoryCreateForm addCategory={addCategory}></CategoryCreateForm>
+  :
+  <CategoryEditForm key={currentCategory._id} category={currentCategory} UpdateCategory={UpdateCategory}></CategoryEditForm>
 
-    {(!isEdit) ?
-    <CategoryCreateForm addCategory={addCategory} setFile={setFile} user={props.user}/>
+  }
+  
+</div>
     
-     :
-    <CategoryEditForm key={currentCategory._id} category={currentCategory} updatedCategory={updatedCategory} setFile={setFile} user={props.user}/>
-    }
-     </div>
-
-    </>
   )
 }
