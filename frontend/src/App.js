c065e72f4e0321a073a20987a6e8f898265c7ff7
noor <noormahdi205@gmail.com>
2024-01-21 10:44:40 +0300

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ConsultationList from './components/consultation/consultationList'
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
